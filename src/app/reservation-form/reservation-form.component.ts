1beab429235c01bbac8792b18b020240cd3a6fb0
sblsreddy <r14141@gmail.com>
2024-08-19 20:10:38 -0500

    Step Form Validation

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
 export class ReservationFormComponent implements OnInit {
+  reservationForm : FormGroup = new FormGroup({});
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { 
 
+  }
+
+// Life cycle hook
   ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['',Validators.required],
+      checkOutDate: ['',Validators.required],
+      guestName: ['',Validators.required],
+      guestEmail: ['',[Validators.required, Validators.email]],
+      roomNumber: ['',Validators.required]
+    })
+  }
+
+
+  onSubmit(){
+    if(this.reservationForm.valid){
+      console.log("valid");
+    }
   }
 
 }
